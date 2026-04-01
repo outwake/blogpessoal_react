a3363f2c3cd627d7ab075bdc8160e5dbc444b900
Lary <larissa.mendy@gmail.com>
2026-04-01 18:58:14 -0300

    💥 NEW: Implemantação da Navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+
+
+function Navbar() {
+  return (
+    <div className="w-full flex justify-center bg-purple-950 text-white py-4">
+        <div className=" container flex justify-between text-lg mx-8">
+            Blog Pessoal
+            
+        <div className=" flex gap-4 mx-8">
+           Postagens
+           Temas
+           Cadastrar Tema 
+           Perfil 
+           Sair
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
