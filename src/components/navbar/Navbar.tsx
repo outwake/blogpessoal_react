af778b9abd0ff9294a23cff0ca3e7adf3f07c8cd
Lary <larissa.mendy@gmail.com>
2026-04-02 12:14:51 -0300

    👌 IMPROVE: Modificação da Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
 
 
 function Navbar() {
   return (
-    <div className="w-full flex justify-center bg-purple-950 text-white py-4">
+    <div className="w-full flex justify-center bg-indigo-900 text-white py-4">
         <div className=" container flex justify-between text-lg mx-8">
+           <Link to="/home" className="text-2xl font-bold ">
             Blog Pessoal
-            
+            </Link>
         <div className=" flex gap-4 mx-8">
            Postagens
            Temas
