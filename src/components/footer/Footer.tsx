36bc5ec49e7111ec8f546e1eb8a27f00e4b9ff4e
Lary <larissa.mendy@gmail.com>
2026-04-02 12:14:27 -0300

    👌 IMPROVE: Modificação do Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
   let data = new Date().getFullYear()
   return (
     <>
-    <div className="flex justify-center bg-purple-950 text-white">
+    <div className="flex justify-center bg-indigo-900 text-white">
       <div className=" container flex flex-col items-center py-4">
         <p className="text-xl font-bold">
           Blog Pessoal da Lary | Copyright: {data}
