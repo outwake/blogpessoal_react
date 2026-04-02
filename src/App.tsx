80567218c4d733ea37fd8917b03e18453520c702
Lary <larissa.mendy@gmail.com>
2026-04-02 12:13:56 -0300

    💥 NEW:  Modificação da Page principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import Login from "./pages/login/Login"
+import Cadastro from "./pages/cadastro/Cadastro"
 
 
 function App() {
 
   return (
     <>{/** Precisamos fazer uma div fantasma para agrupar os elementos filhos e não cria uma div na dom */}
+   <BrowserRouter> {/* Caminho de rota*/}
    <Navbar/>
-   <Home/>
+  <div className="min-h-[80vh]">
+  <Routes>{/**Quando o elemento tem rota */}
+    <Route path="/" element= {<Login/>} />
+    <Route path="/home" element= {<Home/>} />
+    <Route path="/cadastro" element= {<Cadastro/>} />
+  </Routes>
+  </div>
    <Footer/>
+   </BrowserRouter>
    </>
 
   )
